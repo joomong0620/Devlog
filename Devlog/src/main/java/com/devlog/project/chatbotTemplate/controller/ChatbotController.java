1983259851701af6faa4369aec053d06538ce049
nickName333777 <yypark.rok@gmail.com>
2025-11-30 15:48:00 +0900

    html파일 static 경로 -> templates경로로 이동

--- Devlog/src/main/java/com/devlog/project/chatbotTemplate/controller/ChatbotController.java
+++ Devlog/src/main/java/com/devlog/project/chatbotTemplate/controller/ChatbotController.java
@@ -1,26 +1,36 @@
 package com.devlog.project.chatbotTemplate.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.devlog.project.chatbotTemplate.service.ChatbotService;
 
 import java.util.Map;
 
-@RestController
+//@RestController // @Controller + @ResponseBody // => 아래 @GetMapping("page")에서 경로로 forward가 아니라 경로를 String으로 반환하는 이슈 일으킴
+@Controller
 @RequestMapping("/api/chat")
 public class ChatbotController {
 
     private final ChatbotService chatService;
+    
     public ChatbotController(ChatbotService chatService){ this.chatService = chatService; }
 
+    @GetMapping("page") // chatbotTemplate pop-up window
+    public String chatbotTemplatePage() {
+    	return "chatbotTemplate/chatbotTemplate";
+    }
+    
     @PostMapping("/{sessionId}")
+    @ResponseBody
     //public Map<String,Object> chat(@PathVariable("sessionId")  String sessionId, @RequestBody String message){
     public Map<String,Object> chat(@PathVariable  String sessionId, @RequestBody String message){
         return chatService.sendMessage(sessionId, message);
     }
 
     @PostMapping("/lastAnswer")
+    @ResponseBody
     public Map<String,Object> lastAnswer(@RequestBody Map<String,String> payload){
         String sessionId = payload.get("sessionId");
         String question = payload.get("lastQuestion");
