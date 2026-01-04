4fd13fb3119412ef09d70e208ad0330d5cecdc20
nickName333777 <yypark.rok@gmail.com>
2026-01-04 22:10:12 +0900

    FEAT. 자유게시판 댓글 조회/삽입/수정/삭제 기능 추가

---
+++ Devlog/src/main/java/com/devlog/project/board/freeboard/model/mapper/FbCommentMapper.java
@@ -0,0 +1,25 @@
+package com.devlog.project.board.freeboard.model.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.devlog.project.board.freeboard.model.dto.CommentFB;
+
+@Mapper
+public interface FbCommentMapper {
+	
+	// 댓글 목록 조회
+	public List<CommentFB> select(Long boardNo);
+
+	// 댓글 삽입
+	public Long insert(CommentFB comment);
+
+
+	// 댓글 삭제
+	public int delete(CommentFB comment);
+
+	
+	// 댓글 수정
+	public int update(CommentFB comment);
+}
