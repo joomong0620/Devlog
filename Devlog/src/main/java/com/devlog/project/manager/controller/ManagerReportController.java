75de005e74a16605937c1c2ca71443176c1c2be7
Soyeon <homepot@naver.com>
2026-01-04 18:50:53 +0900

    Feat. 신고 중간 커밋

--- Devlog/src/main/java/com/devlog/project/manager/controller/ManagerReportController.java
+++ Devlog/src/main/java/com/devlog/project/manager/controller/ManagerReportController.java
@@ -1,29 +1,34 @@
-package com.devlog.project.manager.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequestMapping("/manager/report")
-@RequiredArgsConstructor
-public class ManagerReportController {
-
-    private final AdminReportService reportService;
-
-    @GetMapping
-    public String reportList(Model model) {
-        model.addAttribute("reportList", reportService.findAllReports());
-        return "manager/manager-report";
-    }
-
-    @PostMapping("/status")
-    @ResponseBody
-    public void updateStatus(
-        @RequestParam Long reportId,
-        @RequestParam String status
-    ) {
-        reportService.updateStatus(reportId, status);
-    }
-}
+//package com.devlog.project.manager.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Controller
+//@RequestMapping("/manager/report")
+//@RequiredArgsConstructor
+//public class ManagerReportController {
+//
+//    private final AdminReportService reportService;
+//
+//    @GetMapping
+//    public String reportList(Model model) {
+//        model.addAttribute("reportList", reportService.findAllReports());
+//        return "manager/manager-report";
+//    }
+//
+//    @PostMapping("/status")
+//    @ResponseBody
+//    public void updateStatus(
+//        @RequestParam Long reportId,
+//        @RequestParam String status
+//    ) {
+//        reportService.updateStatus(reportId, status);
+//    }
+//}
